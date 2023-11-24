9f2ed06445c4275071fcc658b525d3b4f12994a2
KTthecoder <ktthecoder@gmail.com>
2023-11-24 13:33:08 +0100

    Search Groups

--- src/app/search/[search]/page.tsx
+++ src/app/search/[search]/page.tsx
@@ -57,15 +57,20 @@ const page = async (props: Props) => {
             }
         }
     }, orderBy: {createdAt: 'desc'}})
+    const groups = await prisma.groups.findMany({where: {OR: [{name: {contains: props.params.search}}, {description: {contains: props.params.search}}]}, orderBy: {createdAt: 'desc'}, include: {
+        _count: {select: {
+          UserInGroup: true
+        }}
+    }, take: 3})
 
-    const groups = await prisma.groups.findMany({where: {UserInGroup: {some: {usersId: user?.id}}}, include: {
+    const groupsCount = await prisma.groups.findMany({where: {UserInGroup: {some: {usersId: user?.id}}}, include: {
         _count: {select: {UserInGroup: true}}
     }})
 
     return (
         <main className='w-full flex flex-row items-center justify-center'>
             <div className='w-10/12 flex flex-row justify-center mt-24 max-w-[1700px] lg:justify-between lg:mt-28'>
-            <DrawerNavLeft groups={groups} user={{username: user?.username, profileImg: user?.profileImg?.toString(), profileImgAlt: user?.profileImgAlt?.toString()}}/>
+            <DrawerNavLeft groups={groupsCount} user={{username: user?.username, profileImg: user?.profileImg?.toString(), profileImgAlt: user?.profileImgAlt?.toString()}}/>
             <div className='flex flex-col w-full md:w-[600px] lg:w-7/12 xl:w-5/12'>
                 <div className='flex flex-col mb-10'>
                     <h1 className='text-2xl tracking-wider pt-3 mb-3 flex items-center'><IoMdSearch size={25} className='mr-3'/> Search results</h1>
@@ -74,10 +79,13 @@ const page = async (props: Props) => {
                 <div className='flex flex-col mb-7 border-b border-b-[#111] pb-5'>
                     <h1 className='text-2xl tracking-wider pt-3 flex items-center'><GrGroup size={25} className='mr-3'/>Groups</h1>
                 </div>
-                {/* <GroupHorizontal/>
-                <GroupHorizontal/>
-                <GroupHorizontal/> */}
-                <Link className='bg-[#0a0a0a] rounded-md py-2 text-center mb-5 mt-2' href='/search/groups/search-text'>Load more</Link>
+                {groups.length === 0 ? <h1 className='-mt-2 mb-5'>You are in 0 groups</h1> : 
+                <>
+                {groups.map((item, key) => (
+                    <GroupHorizontal image='' id={item.id} description={item.description} name={item.name} usersCount={item._count} visible={item.status} key={key}/>
+                ))}
+                <Link className='bg-[#0a0a0a] rounded-md py-2 text-center mb-5 mt-2' href={`/search/groups/${props.params.search}`}>Load more</Link>
+                </>}
                 <div className='flex flex-col mb-7 border-b border-b-[#111] pb-5'>
                     <h1 className='text-2xl tracking-wider pt-3 flex items-center'><LuUsers size={25} className='mr-3'/>People</h1>
                 </div>
@@ -88,11 +96,15 @@ const page = async (props: Props) => {
                 <div className='flex flex-col mb-10 border-b border-b-[#111] pb-5 mt-2'>
                     <h1 className='text-2xl tracking-wider pt-3 flex items-center'><MdOutlineArticle size={25} className='mr-3'/> Posts</h1>
                 </div>
+                {posts.length === 0 ? <h1 className='-mt-5'>Can't find posts</h1> : 
+                <>
                 {posts.map((item, key) => (
                     <Article userId={item.user.id} comments={item.PostComments} key={key} saved={item.SavedPosts[0] ? item.SavedPosts[0].postsId : ''} id={item.id} createdAt={item.createdAt.toLocaleDateString().toString()} username={item.user.username} description={item.description?.toString()} 
                     likes={item.likes} disLikes={item.disLikes}/>
                 ))}
                 <Link className='bg-[#0a0a0a] rounded-md py-2 text-center mb-5 mt-2' href={`/search/posts/${props.params.search}`}>Load more</Link>
+                </>}
+                
             </div>
             <div className="hidden xl:flex flex-col lg:w-3/12 lg:max-w-[270px]"></div>
             </div>
--- src/app/search/groups/[search]/page.tsx
+++ src/app/search/groups/[search]/page.tsx
@@ -12,7 +12,11 @@ import prisma from '@/lib/db';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
 
-type Props = {}
+type Props = {
+    params: {
+        search: string
+    }
+}
 
 export const metadata: Metadata = {
   title: 'Search | SocialMediaApp',
@@ -28,27 +32,34 @@ const page = async (props: Props) => {
         id: true,
     }})
 
-    const groups = await prisma.groups.findMany({where: {UserInGroup: {some: {usersId: user?.id}}}, include: {
+    const groupsCount = await prisma.groups.findMany({where: {UserInGroup: {some: {usersId: user?.id}}}, include: {
         _count: {select: {UserInGroup: true}}
     }})
 
+    const groups = await prisma.groups.findMany({where: {OR: [{name: {contains: props.params.search}}, {description: {contains: props.params.search}}]}, orderBy: {createdAt: 'desc'}, include: {
+        _count: {select: {
+          UserInGroup: true
+        }}
+    }})
     
     return (
         <main className='w-full flex flex-row items-center justify-center'>
             <div className='w-10/12 flex flex-row justify-center mt-24 max-w-[1700px] lg:justify-between lg:mt-28'>
-            <DrawerNavLeft groups={groups} user={{username: user?.username, profileImg: user?.profileImg?.toString(), profileImgAlt: user?.profileImgAlt?.toString()}}/>
+            <DrawerNavLeft groups={groupsCount} user={{username: user?.username, profileImg: user?.profileImg?.toString(), profileImgAlt: user?.profileImgAlt?.toString()}}/>
             <div className='flex flex-col w-full md:w-[600px] lg:w-7/12 xl:w-5/12'>
                 <div className='flex flex-col mb-10'>
                     <h1 className='text-2xl tracking-wider pt-3 mb-3 flex items-center'><IoMdSearch size={25} className='mr-3'/> Search results</h1>
-                    <p className='text-gray-300 tracking-wide border-t border-t-[#111] pt-3'><span className='font-medium text-gray-200'>Found for: </span>text to search</p>
+                    <p className='text-gray-300 tracking-wide border-t border-t-[#111] pt-3'><span className='font-medium text-gray-200'>Found for: </span>{props.params.search}</p>
                 </div>
                 <div className='flex flex-col mb-7 border-b border-b-[#111] pb-5'>
                     <h1 className='text-2xl tracking-wider pt-3 flex items-center'><GrGroup size={25} className='mr-3'/>Groups</h1>
                 </div>
-                {/* <GroupHorizontal/>
-                <GroupHorizontal/>
-                <GroupHorizontal/> */}
-                <Link className='bg-[#0a0a0a] rounded-md py-2 text-center mb-5 mt-2' href='/'>Load more</Link>
+                {groups.length === 0 ? <h1 className='-mt-2 mb-5'>You are in 0 groups</h1> : 
+                <>
+                {groups.map((item, key) => (
+                    <GroupHorizontal image='' id={item.id} description={item.description} name={item.name} usersCount={item._count} visible={item.status} key={key}/>
+                ))}
+                </>}
             </div>
             <div className="hidden xl:flex flex-col lg:w-3/12 lg:max-w-[270px]"></div>
             </div>
