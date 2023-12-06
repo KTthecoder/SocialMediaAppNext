3e48d20c00a7e4fe6230e5d3c1b073060f2d5ce0
KTthecoder <ktthecoder@gmail.com>
2023-12-06 21:58:41 +0100

    fixed

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,12 +4,6 @@ import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import prisma from "./db";
 
-type ReqType = {
-    headers: {
-        host: string
-    }
-}
-
 export const authOptions:NextAuthOptions = {
     adapter: PrismaAdapter(prisma),
     session: {
